9c841f0477ed337e50ed075eddf4a63bc0802a95
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-09-20 01:09:42 +0530

    changing type of message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send({ id: 1, name: "Harshita" });
 });
 app.get("/messages", (req, res) => {
   res.send("<ul><li>Hiiii!!!</li></ul>");
