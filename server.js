7d6f8c50ddde88fc5ebb165b714aed3f1b1e5425
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-09-20 03:55:50 +0530

    routing

--- server.js
+++ server.js
@@ -4,14 +4,37 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+  {
+    id: 0,
+    name: "A",
+  },
+  {
+    id: 1,
+    name: "B",
+  },
+  {
+    id: 2,
+    name: "C",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send({ id: 1, name: "Harshita" });
 });
-app.get("/messages", (req, res) => {
-  res.send("<ul><li>Hiiii!!!</li></ul>");
+app.get("/friends", (req, res) => {
+  res.json(friends);
 });
-app.post("/messages", (req, res) => {
-  console.log("Updating...");
+app.get("/friends/:fId", (req, res) => {
+  const fId = Number(req.params.fId);
+  const friend = friends[fId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist!",
+    });
+  }
 });
 
 app.listen(PORT, () => {
