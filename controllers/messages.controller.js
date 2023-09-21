c37c97b32c00e9948d745936d62ba2dc8a59229d
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-09-21 21:48:50 +0530

    sending files

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 function getMessages(req, res) {
-  return res.send("<ul><li>HIIIII!!!</li></ul>");
+  res.sendFile(path.join(__dirname, "..", "public", "flowers.jpeg"));
+  // return res.send("<ul><li>HIIIII!!!</li></ul>");
 }
 function postMessages(req, res) {
   console.log("Updating...");
