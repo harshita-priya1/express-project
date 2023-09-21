a1874d4d4653df883e2bd7ba2e63dc1036586533
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-09-21 22:38:12 +0530

    static rendering, layouts and separating concerns

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require("path");
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, "..", "public", "images", "flowers.jpeg"));
+  res.render("messages", {
+    title: "Messages to my friends!",
+    friend: "Harshita",
+  });
+  // res.sendFile(path.join(__dirname, "..", "public", "images", "flowers.jpeg"));
   // return res.send("<ul><li>HIIIII!!!</li></ul>");
 }
 function postMessages(req, res) {
